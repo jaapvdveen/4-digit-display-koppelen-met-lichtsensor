6428871a01932862bd110ddfd740acc65daf53f7
jaapvdveen <jaapvdveen@users.noreply.github.com>
2023-12-15 11:42:13 +0100

    het werkt nu.

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 let lichtsensor = pins.analogReadPin(AnalogPin.P0)
+let display = grove.createDisplay(DigitalPin.P2, DigitalPin.P16)
 basic.forever(function () {
     lichtsensor = pins.analogReadPin(AnalogPin.P0)
     serial.writeValue("x", lichtsensor)
+    display.show(lichtsensor)
     basic.pause(50)
 })
