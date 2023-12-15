8cdc3a0911bdff480d51a024f1ed951c8b9e9283
jaapvdveen <jaapvdveen@users.noreply.github.com>
2023-12-15 11:44:32 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+/**
+ * Let er op dat je de lichtsensor aansluit op P0, en het display aansluit op P2!
+ */
 let lichtsensor = pins.analogReadPin(AnalogPin.P0)
 let display = grove.createDisplay(DigitalPin.P2, DigitalPin.P16)
 basic.forever(function () {
